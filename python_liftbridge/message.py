a849fd5b2ac97ee1ed48b400c47bb7814b69c7e2
Tung Hoang <sontunghoang@gmail.com>
2020-07-04 15:03:34 +0200

    WIP: add metadata class

--- python_liftbridge/message.py
+++ python_liftbridge/message.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from logging import getLogger
 from logging import NullHandler
 
---
+++ python_liftbridge/metadata.py
@@ -0,0 +1,84 @@
+# -*- coding: utf-8 -*-
+
+import NamedTuple
+import random
+from logging import getLogger
+
+logger = getLogger(__name__)
+
+
+class BrokerInfo(NamedTuple):
+    id: str
+    host: str
+    port: int
+
+
+class PartitionInfo(NamedTuple):
+    id: int
+    leader: BrokerInfo
+    replicas: list
+    isr: list
+
+
+class StreamInfo(NamedTuple):
+    subject: str
+    name: str
+    partitions: list
+
+
+class MetaData(object):
+    def __init__(self, brokers, streams, addr):
+        self.brokers = list
+        self.streams = list
+        self.addr = list
+
+    @staticmethod
+    def find_broker_by_id(entries, id):
+        return next(x for x in entries if x.id == id)
+
+    @staticmethod
+    def find_partition_by_id(entries, id):
+        return next(x for x in entries if x.id == id)
+
+    @staticmethod
+    def find_broker_addr(self, metadata, stream_name, partition,
+                         read_isr_replica):
+        stream = next(x for x in metadata.streams if x.name == stream_name)
+        partition_of_stream = MetaData.find_partition_by_id(
+            stream.partitions, partition)
+        if not read_isr_replica:
+            leader = partition_of_stream.leader
+            leader_info = MetaData.find_broker_by_id(metadata.brokers, leader)
+            return leader_info.host + ":" + str(leader_info.port)
+        random_isr = random.choice(partition_of_stream.isr)
+        return random_isr.host + ":" + str(random_isr.port)
+
+    def refresh_meta_data(self, meta_data_response):
+        streams = []
+        # Broker
+        brokers = []
+        addr = []
+        for broker in meta_data_response.brokers:
+            b = BrokerInfo(id=broker.id, port=broker.port, host=broker.host)
+            brokers.append(b)
+            addr.append(b.host + ":" + str(b.port))
+
+        for stream_meta in meta_data_response.metadata:
+            partitions = []
+
+            for _, p in stream_meta.partitions.items():
+                replicas = [
+                    self.find_broker_by_id(brokers, r) for r in p.replicas
+                ]
+                isr = [self.find_broker_by_id(brokers, r) for r in p.isr]
+                partition_info = PartitionInfo(id=p.id,
+                                               leader=p.leader,
+                                               replicas=replicas,
+                                               isr=isr)
+                partitions.append(partition_info)
+            stream = StreamInfo(stream_meta.subject, stream_meta.name,
+                                partitions)
+            streams.append(stream)
+        self.brokers = brokers
+        self.streams = streams
+        self.addr = addr
